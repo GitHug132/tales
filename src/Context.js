da9c08125b4ac5a01ffa10a39856fe7218b81e90
Andres Rios <rianby64@gmail.com>
2016-03-31 23:03:32 +0300

    Perform an small refactor

--- src/Context.js
+++ src/Context.js
@@ -1,39 +1,40 @@
 
-export function parse(text) {
-  function parseTales(text) {
-    var f_c = text.replace(/#.*/g, ''),
-        item_re = /(?:\n{0,1}(.+))+/g,
-        tab_re = /((?:\n {2}|\n\t{1}).+)+/g,
-        item, tab, current_item,
-        result = [];
+function parseTales(text) {
+  var f_c = text.replace(/#.*/g, ''),
+      item_re = /(?:\n{0,1}(.+))+/g,
+      tab_re = /((?:\n {2}|\n\t{1}).+)+/g,
+      item, tab, current_item,
+      result = [];
 
-    tab = tab_re.exec(f_c);
-    if (!tab) {
-      tab = [];
-    }
-    while ((item = item_re.exec(f_c)) !== null) {
-      if (item.index === item_re.lastIndex) {
-        item_re.lastIndex++;
-      }
-      if (item[0] === tab[0]) {
-        if (tab.index === tab_re.lastIndex) {
-          tab_re.lastIndex++;
-        }
-        current_item.subtale += tab[0].replace(/\n {2}|\n\t{1}/g, '\n') + '\n';
-        tab = tab_re.exec(f_c);
-        if (!tab) {
-          tab = [];
-        }
-      } else {
-        current_item = {
-          tale: item[0],
-          subtale: ""
-        }
-        result.push(current_item);
-      }
-    }
-    return result;
+  tab = tab_re.exec(f_c);
+  if (!tab) {
+    tab = [];
   }
+  while ((item = item_re.exec(f_c)) !== null) {
+    if (item.index === item_re.lastIndex) {
+      item_re.lastIndex++;
+    }
+    if (item[0] === tab[0]) {
+      if (tab.index === tab_re.lastIndex) {
+        tab_re.lastIndex++;
+      }
+      current_item.subtale += tab[0].replace(/\n {2}|\n\t{1}/g, '\n') + '\n';
+      tab = tab_re.exec(f_c);
+      if (!tab) {
+        tab = [];
+      }
+    } else {
+      current_item = {
+        tale: item[0],
+        subtale: ""
+      }
+      result.push(current_item);
+    }
+  }
+  return result;
+}
+
+export function parse(text) {
   var result = [],
       tale, tales,
       title_re = /(.+)/,
